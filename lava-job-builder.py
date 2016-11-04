fcab0c3d9357519672930dcf08d82c228341bf58
qiujieqiong <qiujieqiong@deepin.com>
2016-11-04 10:08:06 +0800

    Exec testplan in testlink, and upload testcase_id, testcase_result, testcase_exec_time

---
+++ lava-job-builder.py
@@ -0,0 +1,252 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import os
+import copy
+import itertools
+import functools
+import json
+import sys
+import argparse
+import urllib.request
+from xmlrpc import client
+from html.parser import HTMLParser
+
+
+class DeviceInfo(HTMLParser):
+
+    def __init__(self, hostname, name):
+        HTMLParser.__init__(self)
+        self.in_tag = False
+        self.tags = []
+        response = urllib.request.urlopen(
+            "https://%s/scheduler/device/%s" % (hostname, name))
+        self.feed(str(response.read().decode('utf-8')))
+
+    def handle_starttag(self, tag, attrs):
+        if tag != "dd":
+            return
+        for attr in attrs:
+            if attr[1] == "tag-name":
+                self.in_tag = True
+
+    def handle_endtag(self, tag):
+        if tag != "dd":
+            return
+
+        if self.in_tag:
+            self.in_tag = False
+
+    def handle_data(self, data):
+        import re
+        if self.in_tag:
+            self.tags.append(re.sub('\\n|\s+', '', data))
+
+
+def cache_device_info(username, hostname, token):
+    s = client.ServerProxy(
+        "https://%s:%s@%s/RPC2" % (username, token, hostname))
+    infos = []
+    for dev in s.scheduler.all_devices():
+        dinfo = DeviceInfo(hostname, dev[0])
+        infos.append({
+            "hostname": dev[0],
+            "tags": dinfo.tags,
+            "status": dev[2],
+        })
+    return infos
+
+
+class JobTemplate:
+
+    def __init__(self, device_infos, kernel, ramdisk, nfsrootfs, submitter):
+        self.device_infos = device_infos
+        self.kernel = kernel
+        self.ramdisk = ramdisk
+        self.nfsrootfs = nfsrootfs
+        self.submitter = submitter
+        self.jobs_by_target = {}
+        self.jobs = []
+
+    def feed(self, data):
+        job = json.loads(data)
+        # split testdef by target
+        for idx, tdef in enumerate(job["testdef_repos"]):
+            mtags = tdef["minimal_tags"]
+            targets = [dinfo["hostname"]
+                       for dinfo in self.device_infos if set(mtags).issubset(dinfo["tags"])]
+            if len(targets) == 0:
+                raise ResourceWarning("Can't match any targets for this job" +
+                                      ", which require %s" % mtags)
+
+            for target in targets:
+                if target not in self.jobs_by_target:
+                    self.jobs_by_target[target] = []
+
+                t = copy.deepcopy(job)
+                new_tdef = copy.deepcopy(tdef)
+                del new_tdef["minimal_tags"]
+                t["testdef_repos"] = [new_tdef]
+                t["idx"] = idx
+                self.jobs_by_target[target].append(t)
+
+    def generate(self):
+        for target, jobs in self.jobs_by_target.items():
+            for k, group in itertools.groupby(jobs, lambda j: j["stream"]):
+                group = list(group)
+                job = copy.deepcopy(group[0])
+
+                for t in group[1:]:
+                    if t.get("timeout", 0) > job.get("timeout", 0):
+                        job["timeout"] = t.get("timeout")
+
+                    job["testdef_repos"] += t["testdef_repos"]
+
+                self.jobs.append(
+                    self.render(
+                        "%s_%s_%d" % (target, job["name"],  job["idx"]),
+                        job["testdef_repos"],
+                        target,
+                        job["stream"],
+                        job.get("timeout", 0),
+                    )
+                )
+
+        return self.jobs
+
+    def render(self, name, test_defs, target, stream, timeout):
+
+        job = {
+            "actions": [
+                {
+                    "command": "deploy_linaro_kernel",
+                    "parameters": {
+                        "bootloadertype": "ipxe",
+                        "kernel": self.kernel,
+                        "ramdisk": self.ramdisk,
+                        "nfsrootfs": self.nfsrootfs,
+                        "target_type": "deepin",
+                    },
+                },
+                {
+                    "command": "boot_linaro_image",
+                },
+                {
+                    "command": "lava_test_shell",
+                    "parameters": {
+                        "testdef_repos": test_defs,
+                    },
+                },
+                {
+                    "command": "submit_results",
+                    "parameters": {
+                        "server": "https://validation.deepin.io/RPC2/",
+                        "stream": stream,
+                    },
+                },
+            ],
+            "timeout": 18000,
+            "job_name": name,
+            "device_type": "x86_skip_ipxe",
+            "target": target,
+        }
+        if timeout > 0:
+            job["actions"][2]["parameters"]["timeout"] = timeout
+        if self.submitter != None:
+            job["submitter"] = self.submitter
+        return job
+
+def parserjson(jsondata):
+    d = json.dumps(jsondata, sort_keys=True, indent=4)
+    print(d)
+
+
+parser = argparse.ArgumentParser(
+    description="Load lava job definition from stdin and overwrite values accord the arguments")
+
+
+parser.add_argument(
+    "--list-devices", help="Dump the lava devices information and exit.", default=False, type=bool)
+
+parser.add_argument(
+    "--nfsrootfs", help="overwrite the actions.deploy_linaro_kernel.parameters.nfsrootfs",
+    default="https://validation.deepin.io/tftpboot/deepin.tar.gz")
+
+parser.add_argument(
+    "--kernel", help="overwrite the actions.deploy_linaro_kernel.parameters.kernel",
+    default="https://validation.deepin.io/tftpboot/vmlinuz")
+
+parser.add_argument(
+    "--ramdisk", help="overwrite the actions.deploy_linaro_kernel.parameters.ramdisk",
+    default="https://validation.deepin.io/tftpboot/initrd.img")
+
+parser.add_argument(
+    "--username", help="the username communicate with lava server",
+    default="admin")
+
+parser.add_argument("--token", help="the token communicate with lava server")
+
+parser.add_argument(
+    "--output", default="./output", help="generate job files")
+
+parser.add_argument(
+    "--hostname", help="the hostname of lava server",
+    default="validation.deepin.io")
+
+parser.add_argument(
+    "--submitter", help="indicator who created this job?", default=None)
+
+parser.add_argument(
+    "job_file", nargs="*", default=sys.stdin, help="read job content from stdin default", type=argparse.FileType("rt"))
+
+parser.add_argument(
+    "--idfile", help="save the test plan case ids filename",
+    default="id.txt")
+
+parser.add_argument(
+    "--templejson", help="the template json file",
+    default="temple.job")
+
+args = parser.parse_args()
+
+dinfos = cache_device_info(args.username, args.hostname, args.token)
+for jdata in dinfos:
+    parserjson(jdata)
+
+if args.list_devices:
+    print(json.dumps(dinfos))
+    print("\n")
+    sys.exit(0)
+
+jt = JobTemplate(dinfos, args.kernel,
+                 args.ramdisk, args.nfsrootfs, args.submitter)
+
+allid = open(args.idfile).read()
+
+allidjson = json.loads(allid)
+docker_id = allidjson["docker_id"]
+lava_id = allidjson["lava_id"]
+
+docker_dict = {}
+docker_dict["version"] = "15.2"
+docker_dict["caseID"] = docker_id
+
+with open("%s/%s.json" % (args.output, "docker_id"), "w") as f:
+    f.write(json.dumps(docker_dict))
+
+f = open(args.templejson)
+jt.feed(f.read())
+
+if not os.path.exists(args.output):
+    os.makedirs(args.output)
+
+for job in jt.generate():
+    locate = job['actions'][2]['parameters']['testdef_repos'][0]
+    locate["parameters"] = {"CASE_ID": lava_id, "AUTO_LOGIN_USER": "deepin"}
+    for key in locate.keys():
+        print(key + "\t:\t" + str(locate[key]))
+
+    parserjson(job)
+
+    with open("%s/%s.json" % (args.output, job["job_name"]), "w") as f:
+        f.write(json.dumps(job))
---
+++ lava-job-submitter.py
@@ -0,0 +1,145 @@
+#!/usr/bin/python3 -u
+# -*- coding: utf-8 -*-
+
+import urllib.request
+import csv
+import sys
+from xmlrpc import client
+import time
+import hashlib
+import codecs
+import argparse
+import json
+import base64
+import threading
+
+
+def wait_output(server, id, max_tries):
+    for i in range(max_tries):
+        try:
+            return server.job_output(id).data.decode()
+        except Exception as e:
+            time.sleep(1)
+            # print("Waiting job {} to run {}s/{}s (E: {})".format(
+            #            id, i, max_tries, e))
+
+
+def show_output_log(server, id):
+    out = wait_output(server, id, 18000)
+    if out == None:
+        raise RuntimeError("Failed wait job %d.." % id)
+    olen = 0
+    end = None
+    out = ""
+
+    def md5(s):
+        m = hashlib.md5()
+        m.update(s.encode())
+        return m.hexdigest()
+    while end is None:
+        nout = server.job_output(id, olen).data.decode()
+        end = server.job_details(id)["end_time"]
+        if md5(nout) != md5(out):
+            out = nout
+            olen = olen + len(out)
+            # print("-----------------", olen, md5(out))
+            # print(out)
+        if end is None:
+            time.sleep(0.5)
+
+
+def show_bundle(server, id):
+    result = False
+    found_custom_test = False
+    try:
+        response = urllib.request.urlopen(
+            "%s/export" % (server.job_details(id)["_results_link"]))
+
+        row_format = "{:>15}" * 3
+        print(row_format.format("test_name", "count_pass", "count_fail"))
+        result = True
+        for row in csv.DictReader(codecs.iterdecode(response, 'utf-8')):
+            if row["test"] != "lava" and row["test"] != None:
+                found_custom_test = True
+            if int(row["count_fail"]) > 0:
+                result = False
+            print(
+                row_format.format(row["test"], row["count_pass"], row["count_fail"]))
+        return result and found_custom_test
+    except Exception as e:
+        print(e)
+        return result and found_custom_test
+
+
+def build_server(args):
+    username = args.user
+    hostname = args.host
+    token = args.token
+    if token == None or hostname == None or username == None:
+        print("Please setup user, server and token arguments")
+        exit(-1)
+
+    return client.ServerProxy(
+        "https://%s:%s@%s/RPC2" % (username, token, hostname))
+
+def download_attach(server, id):
+    job_status = server.scheduler.job_status(id)
+    stream = server.dashboard.get(job_status['bundle_sha1'])
+    for attach in json.loads(stream['content'])['test_runs'][0]['attachments']:
+        if attach['pathname'] == 'test.result':
+            content = base64.b64decode(attach['content']).decode('utf-8')
+            print(content)
+            f = open('lava_result.txt', 'w')
+            f.write(content)
+            f.close()
+            break
+
+
+def thd_lava():
+    parser = argparse.ArgumentParser(
+        description="Submit lava job and output the result"
+    )
+    parser.add_argument(
+        "job_file", nargs="?", default=sys.stdin, help="read job content from stdin default", type=argparse.FileType("rt"))
+    parser.add_argument("--token", help="the lava authentication token")
+    parser.add_argument(
+        "--host", help="the lava api server", default="validation.deepin.io")
+    parser.add_argument(
+        "--user", help="the user to operation with lava api", default="admin")
+
+    args = parser.parse_args()
+
+    server = build_server(args)
+    sched = server.scheduler
+
+    job_content = args.job_file.read()
+    id = sched.submit_job(job_content)
+
+    if id == 0:
+        print("Failed submit job {}".format(job_content))
+
+    print("Submit job to https://{}/scheduler/job/{}".format(args.host, id))
+
+    show_output_log(sched, id)
+
+    if not show_bundle(sched, id):
+        print(
+            "Running failed, see https://{}/scheduler/job/{}".format(args.host, id))
+    download_attach(server, id)
+
+def thd_docker(idfile="output/docker_id.json"):
+    print(idfile)
+    return None
+
+thtsk = []
+
+threaddocker = threading.Thread(target=thd_docker)
+threaddocker.start()
+thtsk.append(threaddocker)
+
+threadlava = threading.Thread(target=thd_lava)
+threadlava.start()
+thtsk.append(threadlava)
+
+for thd in thtsk:
+    thd.join()
