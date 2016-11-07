4038e7a3b9aa081d3a44b963155e9efd63bdbff9
qiujieqiong <qiujieqiong@deepin.com>
2016-11-07 15:22:42 +0800

    Use execution_duration for reportTCResult

--- upload-result.py
+++ upload-result.py
@@ -91,7 +91,7 @@ def reportToTestlink(case_id, case_status, exectime, platform_id):
     # args["buildname"] = BUILDNAME
     args["buildid"] = BUILDID
     args["status"] = case_status
-    args["execduration"] = exectime
+    args["execution_duration"] = exectime
     result = client.reportToTestlink(args)
     print(result)
 
@@ -132,6 +132,6 @@ try:
         else:
             tc_result = 'Fail'
 
-        reportToTestlink(int(tc_id), tc_result[0].lower(), int(tc_exec_time), platform_desktop_id)
+        reportToTestlink(int(tc_id), tc_result[0].lower(), float(tc_exec_time), platform_desktop_id)
 finally:
     ffile.close()
